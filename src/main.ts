b40c4ec2cbdda11e76d63e994bb01512d72ed255
fan-two <j2406234743@outlook.com>
2025-09-21 14:10:26 +0800

    提交

--- src/main.ts
+++ src/main.ts
@@ -2,20 +2,20 @@ import { createOverloadedFunction } from './index';
 import { createExtendType } from './utils';
 
 
-// const fn = createOverloadedFunction<[
-//   () => boolean,
-//   (a: number) => boolean,
-//   (a: string, b: string) => string,
-//   (x: number, y: (n: number) => number) => number,
-//   (d: Promise<number>) => Promise<string>,
-//   (u: { name: string, age: number }) => unknown
-// ]>();
-// fn.addImple(() => true);
-// fn.addImple('number', (a) => a > 0);
-// fn.addImple('string', 'string', (a, b) => a + b);
-// fn.addImple('number', 'function', (x, y) => y(x));
-// fn.addImple('promise', (u) => Promise.resolve(u).then(n => n.toString()));
-// fn.addImple('object', (u) => u.name);
+const fn = createOverloadedFunction<[
+  () => boolean,
+  (a: number) => boolean,
+  (a: string, b: string) => string,
+  (x: number, y: (n: number) => number) => number,
+  (d: Promise<number>) => Promise<string>,
+  (u: { name: string, age: number }) => unknown
+]>();
+fn.addImple(() => true);
+fn.addImple('number', (a) => a > 0);
+fn.addImple('string', 'string', (a, b) => a + b);
+fn.addImple('number', 'function', (x, y) => y(x));
+fn.addImple('promise', (u) => Promise.resolve(u).then(n => n.toString()));
+fn.addImple('object', (u) => u.name);
 
 // console.log(fn());
 // console.log(fn(10));
@@ -48,76 +48,76 @@ import { createExtendType } from './utils';
 // console.log(r1);
 
 
-class User {
-	constructor(public name: string, public age: number, public gender: string) {}
-}
+// class User {
+// 	constructor(public name: string, public age: number, public gender: string) {}
+// }
 
-const extendType = createExtendType({
-	user: User,
-});
+// const extendType = createExtendType({
+// 	user: User,
+// });
 
-const func = createOverloadedFunction<[
-	() => void,
-	(x: number) => number,
-	(x: string, y: string) => string,
-	(x: {
-		name: string;
-		age: number;
-		address: string;
-	}) => boolean,
-	(x: number, y: (n: number) => number) => number,
-	(d: Promise<number>) => string,
-	(u: User) => string,
-	(this: User, n: number, s: string) => boolean
-], typeof extendType>({
-	// allowMultiple: true,
-	extendType
-});	
+// const func = createOverloadedFunction<[
+// 	() => void,
+// 	(x: number) => number,
+// 	(x: string, y: string) => string,
+// 	(x: {
+// 		name: string;
+// 		age: number;
+// 		address: string;
+// 	}) => boolean,
+// 	(x: number, y: (n: number) => number) => number,
+// 	(d: Promise<number>) => string,
+// 	(u: User) => string,
+// 	(this: User, n: number, s: string) => boolean
+// ], typeof extendType>({
+// 	// allowMultiple: true,
+// 	extendType
+// });	
 
-func.addImple(() => {
-	console.log('First implementation for void');
-});
-func();
+// func.addImple(() => {
+// 	console.log('First implementation for void');
+// });
+// func();
 
-func.addImple('number', (x) => {
-	console.log('First implementation for number');
-	return x * 2;
-});
+// func.addImple('number', (x) => {
+// 	console.log('First implementation for number');
+// 	return x * 2;
+// });
 
-func.addImple('string', 'string', (x, y) => {
-	return x + y;
-});
+// func.addImple('string', 'string', (x, y) => {
+// 	return x + y;
+// });
 
-func.addImple('object', (x) => {
-	return x.age > 18;
-});
+// func.addImple('object', (x) => {
+// 	return x.age > 18;
+// });
 
-func.addImple('number', 'function', (x, y) => {
-	return y(x) + 2;
-});
+// func.addImple('number', 'function', (x, y) => {
+// 	return y(x) + 2;
+// });
 
-func.addImple('promise', (d) => {
-	return 'Handled a promise';
-});
+// func.addImple('promise', (d) => {
+// 	return 'Handled a promise';
+// });
 
-func.addImple('user', (u) => {
-	return `User: ${u.name}, Age: ${u.age}`;
-});
+// func.addImple('user', (u) => {
+// 	return `User: ${u.name}, Age: ${u.age}`;
+// });
 
-func.addImple('number', 'string', function(a) {
-	this.name
-	return true;
-});
+// func.addImple('number', 'string', function(a) {
+// 	this.name
+// 	return true;
+// });
 
-const r1 = func();
-const r2 = func(10);
-const r3 = func('hello', ' world');
-const r4 = func({ name: 'Alice', age: 20, address: '123 Main St' });
-const r5 = func(5, n => n * 2);
-const r6 = func(Promise.resolve(42));
-const r7 = func(new User('pink', 23, 'male'));
+// const r1 = func();
+// const r2 = func(10);
+// const r3 = func('hello', ' world');
+// const r4 = func({ name: 'Alice', age: 20, address: '123 Main St' });
+// const r5 = func(5, n => n * 2);
+// const r6 = func(Promise.resolve(42));
+// const r7 = func(new User('pink', 23, 'male'));
 
-console.log(r1, r2, r3, r4, r5, r6, r7);
+// console.log(r1, r2, r3, r4, r5, r6, r7);
 
 
 // const test = createOverloadedFunction<[
